ce412526fe9a87109a8b85560415d3304a264b26
elejedomarygwyneth <151907178+elejedomarygwyneth@users.noreply.github.com>
2024-11-03 04:31:47 +0800

    Update PE2.py

--- LAB3/PostLabSolution2/PE2.py
+++ LAB3/PostLabSolution2/PE2.py
@@ -1,1 +1,29 @@
+import random
 
+def main():
+    students = [
+        Student("Zoe", 19, "B"),
+        Student("Alice", 20, "A"),
+        Student("John", 21, "C"),
+        Student("Bob", 22, "B+"),
+        Student("Emma", 23, "A-")
+    ]
+
+    print("Before Shuffling:")
+    for student in students:
+        print(student)
+
+    random.shuffle(students)
+
+    print("\nAfter Shuffling:")
+    for student in students:
+        print(student)
+
+    students.sort()
+
+    print("\nAfter Sorting:")
+    for student in students:
+        print(student)
+
+if __name__ == "__main__":
+    main()
